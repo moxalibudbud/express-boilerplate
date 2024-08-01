456aae7cfb314922d9d30d112ac97d79c039f330
John <moxalibudbud@gmail.com>
2024-08-01 11:49:20 +0400

    Prepare build script

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 import { config  } from './config';
 
-const server = app.listen(config.PORT, () => {
+app.listen(config.PORT, () => {
   console.info(`Listening to port ${config.PORT}`);
 });
