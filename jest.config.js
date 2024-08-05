efb9ab4f471312aad9a40d6a8bf9cc37494e1989
John <moxalibudbud@gmail.com>
2024-08-05 10:10:14 +0400

    Prepare jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // moduleNameMapper: {
+  //   "^../utils/(.*)$": "<rootDir>/src/utils/$1",
+  //   "^../utils$": "<rootDir>/src/utils",
+  //   "^@file-readers/(.*)$": "<rootDir>/src/file-readers/$1",
+  //   "^@file-readers$": "<rootDir>/src/file-readers"
+  // },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+};
---
+++ src/utils/date.ts
@@ -0,0 +1,4 @@
+import * as moment from 'moment-timezone';
+moment.tz.setDefault(process.env.APP_TIMEZONE);
+
+export default moment;
